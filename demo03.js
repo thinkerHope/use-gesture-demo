713bd803c952dcf00ac98e172e3f92b27cb50a60
thinkerHope <47625056+thinkerHope@users.noreply.github.com>
2020-11-17 21:04:06 +0800

    Update demo03.js
    done!!!

--- demo03.js
+++ demo03.js
@@ -7,8 +7,8 @@ import { useSprings, animated } from 'react-spring'
 
 const width = 50
 const height = 50
-const col = 4
-const index2Pos = index => [~~((index + 1) / col) * width, ((index + 1) % col) * height]
+const col = 4 
+const index2Pos = index => [(index % col) * width, ~~(index / col) * height]
 const pos2Index = ([x, y]) => x * col + y
 
 const fn = (order, down, originalIndex, curIndex, x, y) => index => 
@@ -34,11 +34,14 @@ function DraggableList({ items }) {
   const bind = useDrag(({ args: [originalIndex], movement: [x, y], down }) => {
     let curIndex = order.current.indexOf(originalIndex)
     let curPos = [
-      clamp(Math.round(~~((curIndex + 1) / col) * width + x) , 0, col),
-      clamp(((index + 1) % col) * height, 0, ~~(items.length - 1))
+      clamp(Math.round((~~(curIndex / col) * height + y) / height), 0, items.length - 1),
+      clamp(Math.round(((curIndex % col) * width + x) / width) , 0, col)
     ]
+    console.log('curPos', curPos)
     let nextIndex = pos2Index(curPos)
-    let newOrder = swapI(order.current, curIndex, nextIndex)
+    console.log('nextIndex', nextIndex)
+    let newOrder = swap(order.current, curIndex, nextIndex)
+    console.log('newOrder',newOrder)
     setSprings(fn(newOrder, down, originalIndex, curIndex, x, y))
     if (!down) order.current = newOrder
   })
@@ -49,11 +52,12 @@ function DraggableList({ items }) {
       {springs.map(({ xy, zIndex, shadow, scale }, i) => (
         <animated.div 
           {...bind(i)}
+          key={i}
           style={{
-            transform: xy.to((x, y) => `translate3d(${x}px, ${y}px 0)`),
-            boxShadow: shadow.to(s => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`),
+            transform: xy.interpolate((x, y) => `translateX(${x}px) translateY(${y}px)`),
+            boxShadow: shadow.interpolate(s => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`),
             zIndex,
-            scale,
+            scale
           }}
           children={items[i]}
         />
@@ -63,6 +67,6 @@ function DraggableList({ items }) {
 }
 
 render(
-  <DraggableList items={'1 2 3 4 5 6 7 8 9 10 11'.split(' ')} />, 
+  <DraggableList items={'1 2 3 4 5 6 7 8 9 10 11 12 13'.split(' ')} />, 
   document.getElementById('root')
 )
